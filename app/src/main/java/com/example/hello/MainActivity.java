67573661608930bdb8b48684ad6597b02f47fef5
Maate Martin T <maatemartin@gmail.com>
2020-02-05 13:28:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+   // hello world sample application by
+   //MAATE MARTIN THAWITE  - 2017/BIT/094/PS
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
